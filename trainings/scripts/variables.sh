7966e387594c0fe4c48902adc5ef6be9cdaf99a3
Mouli Veera <08sagittarius@gmail.com>
2022-02-09 09:05:44 +0530

    08-feb-2022

--- trainings/scripts/variables.sh
+++ trainings/scripts/variables.sh
@@ -6,24 +6,5 @@
 # Print all positional args
 #echo $@
 
-
-echo "Please enter your name:"
-read m
-
-
-
-echo "Hello $m, Happy to see you here"
-
-
-echo "Enter your phone Number:"
-read ph
-echo "Enter your DOB:"
-read dob
-
-
-echo "Hellow $m, Here are the details you shared. Plase check and confirm \n
-	NAME: $m\n
-	PHONE: $ph \n
-	DOB: $dob
-	"
+echo $#
 
